58a9f2eb12565d67264528ab5e129d0c6ae7be83
Ashley Craig <ashleycraig1987@gmail.com>
2023-02-07 23:30:09 +0000

    Add basic Go file with signal wait

---
+++ go/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func handleSignal(signals <-chan os.Signal, done chan<- bool) {
+	signal := <-signals
+
+	// TODO: maybe use formatted print instead
+	fmt.Println("signal ", signal, " received")
+
+	done <- true
+}
+
+func cleanup() {
+	fmt.Println("fill me in!")
+}
+
+func main() {
+	// TODO: could we put this stuff in a function
+	signals := make(chan os.Signal, 1)
+
+	// TODO: should we include any more maybe SIGQUIT, SIGABRT, SIGKILL?
+	// is there a way to pass as a slice
+	signal.Notify(signals, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan bool, 1)
+
+	go handleSignal(signals, done)
+
+	fmt.Println("waiting for signal")
+	<-done
+	// Run as goroutine?
+	cleanup()
+}
