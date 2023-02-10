9c3ec6a736bae2cc2c1230356ec8d744ec3236c6
Ashley Craig <ashleycraig1987@gmail.com>
2023-02-10 01:21:01 +0000

    Add basic C program and readme

---
+++ c/README.md
@@ -0,0 +1,9 @@
+Compile with:
+`gcc -Wall main.c -o main`
+
+Run with:
+`./main`
+
+# TODO
+
+1. Make a file writer
---
+++ c/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct test {
+    int num;
+};
+
+int main(void) {
+    // struct test t;
+
+    printf("hello, world\n");
+
+    return 0;
+}
