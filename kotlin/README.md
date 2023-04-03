eae2278ff939bc54499d1e738ed05ed0bb8cdc6d
Ashley Craig <ashleycraig1987@gmail.com>
2023-04-03 19:34:21 +0000

    Add non-Spring Boot Kotlin stuff

---
+++ kotlin/README.md
@@ -0,0 +1,8 @@
+1. Ktor
+
+To run:
+`kotlinc main.kt -include-runtime -d main.jar`
+then
+`java -jar main.jar`
+check this first
+`kotlin -classpath main.jar MainKt`
---
+++ kotlin/scripts/install_jdk_and_compiler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Install Kotlin compiler - kotlinc
+
+# TODO: check JDK is installed, if not install
+
+# Does this include kotlin command too
+if ! command -v kotlinc &> /dev/null; then
+# TODO: check if on macOS
+# brew update
+# brew install kotlin
+
+# TOOD: add Linux installation
+fi
