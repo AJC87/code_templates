33e43063852e55a7a20574d9b28171b65e4adbfb
Ashley Craig <ashleycraig1987@gmail.com>
2025-11-07 01:41:47 +0000

    readme stuff, add table

--- albums_app/README.md
+++ albums_app/README.md
@@ -2,3 +2,7 @@
 
 1. Come up with a better name and make new repository
 2. Check it all works!
+3. Look over config files
+4. DynamoDB seeding/clear script
+5. Get cdk bootstrap working
+6. cdk destroy didn't get rid of stack, add CloudFormation command to do it
--- albums_app/cdk/lib/stack.ts
+++ albums_app/cdk/lib/stack.ts
@@ -1,13 +1,19 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new sqs.Queue(this, 'Queue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // new sqs.Queue(this, 'Queue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    const table = new dynamodb.TableV2(this, 'Table', {
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
     });
   }
 }
